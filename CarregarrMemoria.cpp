136fa6b2ad45c685e44a4ea00fa5aebe2a04ba82
julianafgusmao <julianafloresgusmao@gmail.com>
2025-03-28 19:49:28 -0300

    Add files via upload

---
+++ CarregarrMemoria.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char dados[9];
+} MemoriaDados;
+
+void carregarMemoria(MemoriaDados *memdados, const char *ArquivoMemoria){
+    int i;
+    FILE *arquivo = fopen("ArquivoMemoria.txt", "r");
+    if (arquivo == NULL){
+        fprintf(stderr, "Erro ao abrir o arquivo %s\n", ArquivoMemoria);
+        return;
+    }
+    printf("Carregando memória de dados...\n");
+    for (i = 0; i < 256; i++){
+        if (fgets(memdados[i].dados, 9, arquivo) == NULL){
+            break;
+        }
+        memdados[i].dados[strcspn(memdados[i].dados, "\n")] = '\0';
+    }
+    fclose(arquivo);
+    printf("Código carregado com sucesso na memória de dados!\n");
+}
+
+int main(){
+    int i;
+    MemoriaDados memoria[256];
+    carregarMemoria(memoria, "ArquivoMemoria.txt");
+
+    printf("\nConteúdo da memória de dados:\n");
+    for (i = 0; i < 256 && memoria[i].dados[0] != '\0'; i++) {
+        printf("Linha %d: %s\n", i, memoria[i].dados);
+    }
+
+    return 0;
+}
