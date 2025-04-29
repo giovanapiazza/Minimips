66b59a82867f2211567e6f26e0cfa1284a19bfbd
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-04-29 10:49:56 -0300

    Update Multiciclo.c

--- Multiciclo.c
+++ Multiciclo.c
@@ -1,1 +1,127 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 
+
+//voids que serão reaproveitados
+
+Instrucao decod(char* inst) {
+    Instrucao i;
+    char buffer[17];
+
+    strncpy(buffer, inst, 4);
+    buffer[4] = '\0';
+    i.opcode = strtol(buffer, NULL, 2);
+
+    if (i.opcode == 0) {
+        i.tipo = 1;
+        strncpy(buffer, inst + 4, 3); buffer[3] = '\0';
+        i.rs = strtol(buffer, NULL, 2);
+
+        strncpy(buffer, inst + 7, 3); buffer[3] = '\0';
+        i.rt = strtol(buffer, NULL, 2);
+
+        strncpy(buffer, inst + 10, 3); buffer[3] = '\0';
+        i.rd = strtol(buffer, NULL, 2);
+
+        strncpy(buffer, inst + 13, 3); buffer[3] = '\0';
+        i.funct = strtol(buffer, NULL, 2);
+    } else if (i.opcode == 2) {
+        i.tipo = 3;
+        strncpy(buffer, inst + 4, 12); buffer[12] = '\0';
+        i.address = strtol(buffer, NULL, 2);
+    } else {
+        i.tipo = 2;
+        strncpy(buffer, inst + 4, 3); buffer[3] = '\0';
+        i.rs = strtol(buffer, NULL, 2);
+
+        strncpy(buffer, inst + 7, 3); buffer[3] = '\0';
+        i.rt = strtol(buffer, NULL, 2);
+
+        strncpy(buffer, inst + 10, 6); buffer[6] = '\0';
+        int imm = strtol(buffer, NULL, 2);
+        if (inst[10] == '1') imm -= 64;
+        i.immediate = imm;
+    }
+
+    return i;
+}
+
+
+void imprimir_instrucao(Instrucao i) {
+    printf("Tipo: %d | Opcode: %d | RS: %d | RT: %d | RD: %d | Funct: %d | Immediate: %d | Address: %d\n",
+        i.tipo, i.opcode, i.rs, i.rt, i.rd, i.funct, i.immediate, i.address);
+}
+
+
+int ula(int a, int b, int op) { 
+    switch (op) { 
+        case 0: return a + b;  
+        case 1: return a - b;  
+        case 2: return a & b;  
+        case 3: return a | b;  
+        default: printf("Operacao nao reconhecida: %d\n", op); return 0; 
+    } 
+} 
+
+int mux(int a, int b, int select) {
+    return (select == 0) ? a : b;
+}
+
+int check_overflow(int result) {
+    if (result < -128 || result > 127) {
+        return 1;
+    }
+    return 0;
+}
+
+void conv_asm(char *bin_inst, FILE *arquivo_asm, Instrucao inst) {
+    if(inst.rd == 0 && inst.rt == 0 && inst.rs == 0){
+        return;
+    }
+    switch (inst.tipo) {
+        case 1:
+            switch (inst.funct) {
+                case 0:
+                    fprintf(arquivo_asm, "add $r%d, $r%d, $r%d", inst.rd, inst.rs, inst.rt);
+                    break;
+                case 2:
+                    fprintf(arquivo_asm, "sub $r%d, $r%d, $r%d", inst.rd, inst.rs, inst.rt);
+                    break;
+                case 4:
+                    fprintf(arquivo_asm, "and $r%d, $r%d, $r%d", inst.rd, inst.rs, inst.rt);
+                    break;
+                case 5:
+                    fprintf(arquivo_asm, "or $r%d, $r%d, $r%d", inst.rd, inst.rs, inst.rt);
+                    break;
+                default:
+                    fprintf(arquivo_asm, "Funcao R nao reconhecida: %d", inst.funct);
+                    break;
+            }
+            break;
+        case 2:
+            switch (inst.opcode) {
+                case 4:
+                    fprintf(arquivo_asm, "addi $r%d, $r%d, %d", inst.rt, inst.rs, inst.immediate);
+                    break;
+                case 11:
+                    fprintf(arquivo_asm, "lw $r%d, %d($r%d)", inst.rt, inst.immediate, inst.rs);
+                    break;
+                case 15:
+                    fprintf(arquivo_asm, "sw $r%d, %d($r%d)", inst.rt, inst.immediate, inst.rs);
+                    break;
+                case 8:
+                    fprintf(arquivo_asm, "beq $r%d, $r%d, %d", inst.rt, inst.rs, inst.immediate);
+                    break;
+                default:
+                    fprintf(arquivo_asm, "Opcode I nao reconhecido: %d", inst.opcode);
+                    break;
+            }
+            break;
+        case 3:
+            fprintf(arquivo_asm, "j %d", inst.address);
+            break;
+    }
+
+}
